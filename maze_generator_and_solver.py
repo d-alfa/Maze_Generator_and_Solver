255a9e995c0e5906c929d411890e8c508c96257c
d_alfa <dargis.alfonsas@gmail.com>
2024-01-12 21:26:40 +0200

    Create Constructor

---
+++ maze_generator_and_solver.py
@@ -0,0 +1,14 @@
+
+from tkinter import Tk, BOTH, Canvas
+
+class WINDOW:
+
+    def __init__(self, width, height, title):
+        self.__root = Tk() # Creates the new root widget
+        self.__root.geometry(f"{width}x{height}") # Sets the size of the window using the width and height parameters
+        self.__root.title(title) # Set the window title
+
+        self.__canvas = Canvas(self.__root) # Create the Canvas
+        self.__canvas.pack(fill=BOTH, expand=1) # Make the Canvas fill the window
+
+        self.__isRunning = False # Running state attribute
