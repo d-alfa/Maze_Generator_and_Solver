471a1a2c102c07994b85eca90256191ede2b4e01
d_alfa <dargis.alfonsas@gmail.com>
2024-01-13 19:12:51 +0200

    Create Line Class

--- graphics.py
+++ graphics.py
@@ -28,3 +28,8 @@ class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
