0dccc65956d8c5eab1d9efec28882a65c5084c31
d_alfa <dargis.alfonsas@gmail.com>
2024-01-13 22:54:29 +0200

    Create draw function for Cell class

--- cell.py
+++ cell.py
@@ -13,3 +13,21 @@ class Cell:
         self._x2 = None
         self._y2 = None
         self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1,y1), Point(x1,y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1,y1), Point(x2,y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2,y1), Point(x2,y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1,y2), Point(x2,y2))
+            self._win.draw_line(line)
