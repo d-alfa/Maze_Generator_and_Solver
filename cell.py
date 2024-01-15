e64f8061ab8ef4a3a27e0b93c67c9a199e354aa3
d_alfa <dargis.alfonsas@gmail.com>
2024-01-15 19:20:16 +0200

    Remove code, refactor into two different features

--- cell.py
+++ cell.py
@@ -7,7 +7,6 @@ class Cell:
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
@@ -24,27 +23,15 @@ class Cell:
         if self.has_left_wall:
             line = Line(Point(x1,y1), Point(x1,y2))
             self._win.draw_line(line)
-        else:
-            line = Line(Point(x1,y1), Point(x1,y2))
-            self._win.draw_line(line, "white")
         if self.has_top_wall:
             line = Line(Point(x1,y1), Point(x2,y1))
-            self._win.draw_line(line)
-        else:
-            line = Line(Point(x1,y1), Point(x2,y1))
-            self._win.draw_line(line, "white")   
+            self._win.draw_line(line) 
         if self.has_right_wall:
             line = Line(Point(x2,y1), Point(x2,y2))
             self._win.draw_line(line)
-        else:
-            line = Line(Point(x2,y1), Point(x2,y2))
-            self._win.draw_line(line, "white")
         if self.has_bottom_wall:
             line = Line(Point(x1,y2), Point(x2,y2))
             self._win.draw_line(line)
-        else:
-            line = Line(Point(x1,y2), Point(x2,y2))
-            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell, undo=False):
         if self._win is None:
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 from graphics import Window
 from maze import Maze
-import sys
 
 def main():
     num_rows = 12
@@ -11,76 +10,10 @@ def main():
     screen_y = 600
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
-
-    sys.setrecursionlimit(10000)
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
-    print("Maze created")
-    is_solvable = maze.solve()
-    if not is_solvable:
-        print("Maze can not be solved!")
-    else:
-        print("Maze solved!")
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 
 main()
-
-    # Draw 1 line
-    # win = Window(800, 600)
-    # l = Line(Point(50,50), Point(400,400))
-    # win.draw_line(l, "black")
-
-    # win.wait_for_close()
-
-# =======================================
-
-    # Draw 4 cells
-    # c = Cell(win)
-    # c.has_left_wall = False
-    # c.draw(50, 50, 100, 100)
-
-    # c = Cell(win)
-    # c.has_right_wall = False
-    # c.draw(125, 125, 200, 200)
-
-    # c = Cell(win)
-    # c.has_bottom_wall = False
-    # c.draw(225, 225, 250, 250)
-
-    # c = Cell(win)
-    # c.has_top_wall = False
-    # c.draw(300, 300, 500, 500)
-
-    # win.wait_for_close()
-
-# =======================================
-
-    # Connects 4 cells
-    # win = Window(800, 600)
-
-    # c1 = Cell(win)
-    # c1.has_right_wall = False
-    # c1.draw(50, 50, 100, 100)
-
-    # c2 = Cell(win)
-    # c2.has_left_wall = False
-    # c2.has_bottom_wall = False
-    # c2.draw(100, 50, 150, 100)
-
-    # c1.draw_move(c2)
-
-    # c3 = Cell(win)
-    # c3.has_top_wall = False
-    # c3.has_right_wall = False
-    # c3.draw(100, 100, 150, 150)
-
-    # c2.draw_move(c3)
-
-    # c4 = Cell(win)
-    # c4.has_left_wall = False
-    # c4.draw(150, 100, 200, 150)
-
-    # c3.draw_move(c4, True)
-
-    # win.wait_for_close()
