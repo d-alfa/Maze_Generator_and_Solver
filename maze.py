790ab9bc48711339e5c233eba4888b67fd8f4c2e
d_alfa <dargis.alfonsas@gmail.com>
2024-01-15 20:19:17 +0200

    Create _break_entrance_and_exit function

--- maze.py
+++ maze.py
@@ -15,6 +15,7 @@ class Maze:
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -41,3 +42,9 @@ class Maze:
             return
         self._win.redraw()
         time.sleep(0.03)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
